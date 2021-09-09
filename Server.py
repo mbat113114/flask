b5a5713b712b569dbe02ca362dc80e538d67fe15
mbat113114 <77671050+mbat113114@users.noreply.github.com>
2021-09-09 09:39:20 +0530

    update Server.py

--- Server.py
+++ Server.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import Flask ,redirect ,render_template ,url_for
 app = Flask(__name__) 
 
 @app.route("/")
 def home():
-  return "<h1>aman</h1>"
+  return render_template("index.html")
 
 if __name__ =="__main__":
   app.run()
